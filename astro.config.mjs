c7e44c740fa156fae083d1af7a7acaf1f28d9220
Freda Q <hongdao@FredadeMacBook-Air.local>
2026-06-04 10:40:34 +0800

    Switch GitHub Pages to custom domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://freda-q.github.io",
-  base: "/hanlongmarine/",
+  site: "https://new.hanlongmarine.com",
+  base: "/",
   integrations: [tailwind()],
   output: "static"
 });
